41a49c3984195c8f68047ab8693bb9d2c2df9472
Epchk <curiousities@gmail.com>
2024-09-18 10:08:47 -0400

    Security Fix, ensure URL is a url

--- tablo_lite.js
+++ tablo_lite.js
@@ -282,9 +282,10 @@ function doSettings() {
 function bootstrap() {
 	const tablo_ip_addr = getCookie('tablo_ip_addr');	
 	const tv_listings_url = getCookie('tv_listings_url');
-	if (tv_listings_url) {
-		document.getElementById("tv_listings_link").href = tv_listings_url;
-		document.getElementById("tv_listings_url").value = tv_listings_url;
+	const listings_url = RegExp("^http(s)?:\/\/[a-zA-Z0-9;,\/\?:@&=+$_!~*'.-]+$").test(tv_listings_url) ? tv_listings_url : '#Setup';
+	if (listings_url) {
+		document.getElementById("tv_listings_link").href = listings_url;
+		document.getElementById("tv_listings_url").value = listings_url;
 	}
 	
 	if (tablo_ip_addr) {
