57bc323b908e9d5f9e01aae13df9ed98a69ccaf2
epckh <curiousities@gmail.com>
2025-04-30 17:47:10 -0400

    added images

--- tablo_lite.js
+++ tablo_lite.js
@@ -191,7 +191,7 @@ async function doRecordings(page = 1) {
 				const tablo_ip = document.getElementById("tablo_ip_entry").value;
 				const image_url = `http://${tablo_ip}:18080/pvr/${recording.object_id}/snap.jpg`;
 				document.getElementById('Trecordings').innerHTML += `<tr><td>${i} - ${recording.object_id}</td><td>` +
-					`<a href="#watch" onClick="doWatch('${recording.path}');">${recording.airing_details.show_title}</a><br><img src="${image_url}"></img></td><td>` + 
+					`<a href="#watch" onClick="doWatch('${recording.path}');">${recording.airing_details.show_title}</a><br><img src="${image_url}" width="160"></img></td><td>` + 
 					Math.round(recording.video_details.duration/60) + "m </td><td>" +
 					`${recording.video_details.clean}</td><td>` +
 					`${recording.video_details.height}</td><td>` +
